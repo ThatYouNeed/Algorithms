9cd7f290788eb3e303b78ca58b0035e0afee20de
ThatYouNeed <132047327+ThatYouNeed@users.noreply.github.com>
2024-07-28 15:35:32 +0530

    Update Fib_With_List.py

--- Fib_With_List.py
+++ Fib_With_List.py
@@ -15,10 +15,12 @@ def fibwith(list,n):
 
     while left <= right:
         mid = (left+right)//2
-        if mid < n:
-            left += 1
-        elif mid > n:
-            right -= 1
+        if list[mid] < n:
+            left = mid + 1
+        elif list[mid] > n:
+            right = mid - 1
+        else:
+            return list[mid-1] if mid>0 else None
     return list[right] if right >=0 else None
 
 list = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]
