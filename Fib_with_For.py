e26866539da89687a96b6373ea140ca0334ff87c
ThatYouNeed <132047327+ThatYouNeed@users.noreply.github.com>
2024-08-01 19:11:08 +0530

    Update Fib_with_For.py

--- Fib_with_For.py
+++ Fib_with_For.py
@@ -1,16 +1,32 @@
+# n = int(input('Enter: '))
+
+# n1,n2 = 0,1
+
+# if n==0:
+#     print("Check the Number")
+# if n>=1:
+#     print(n1,end = " ")
+# if n>=2:
+#     print(n2,end = " ")
+
+# for i in range(2,n):
+#     n3 = n1+n2
+#     n1 = n2
+#     n2 = n3
+#     print(n3,end=" ")
+
 n = int(input('Enter: '))
 
-n1,n2 = 0,1
+n1, n2 = 0, 1
 
-if n==0:
+if n == 0:
     print("Check the Number")
-if n>=1:
-    print(n1,end = " ")
-if n>=2:
-    print(n2,end = " ")
+elif n == 1:
+    print(n1)
+else:
+    print(n1, end=" ")
+    print(n2, end=" ")
 
-for i in range(2,n):
-    n3 = n1+n2
-    n1 = n2
-    n2 = n3
-    print(n3,end=" ")
+    for i in range(2, n):
+        n1, n2 = n2, n1 + n2
+        print(n2, end=" ")
